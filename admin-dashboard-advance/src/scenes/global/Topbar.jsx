ed08635463a14749b2f992b797306cdd4f1bb5bb
abuzar786 <49241793+abuzar786@users.noreply.github.com>
2023-01-19 22:43:28 +0100

    Added theme cutomization component

--- admin-dashboard-advance/src/scenes/global/Topbar.jsx
+++ admin-dashboard-advance/src/scenes/global/Topbar.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Box, IconButton, useTheme } from "@mui/material";
+import { useContext } from "react";
+import { ColorModeContext, tokens } from "../../theme";
+import InputBase from "@mui/material";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import NotificationsModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import SettingsModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import PersonModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import SearchIcon from "@mui/icons-material/Search";
 
 const topbar = () => {
-  return <div>topbar</div>;
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
+  return <Box display="flex" justifyContent="space-between"></Box>;
 };
 
 export default topbar;
